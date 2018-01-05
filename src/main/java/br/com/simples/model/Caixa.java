105245b0996a3b696471158b61f9a655560c5d24
alanfirmiano <alanfirmiano18@gmail.com>
2018-01-05 16:40:01 -0300

    Caixa model

--- src/main/java/br/com/simples/model/Caixa.java
+++ src/main/java/br/com/simples/model/Caixa.java
@@ -10,5 +10,25 @@ public class Caixa {
 	@Id
 	@GeneratedValue
 	private Integer id;
-	
+	private String numero;
+
+	public Caixa(){
+
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
 }
