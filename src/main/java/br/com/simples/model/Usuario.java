2a5918daf5f0c822c2b85c762d176472f3220751
alanfirmiano <alanfirmiano18@gmail.com>
2018-01-05 16:47:01 -0300

    rebase

--- src/main/java/br/com/simples/model/Usuario.java
+++ src/main/java/br/com/simples/model/Usuario.java
@@ -10,5 +10,38 @@ public class Usuario {
 	@Id
 	@GeneratedValue
 	private Integer id;
+<<<<<<< HEAD
 	
+=======
+	private String nome;
+	private String senha;
+
+	public Usuario(){
+
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+>>>>>>> User update
 }
